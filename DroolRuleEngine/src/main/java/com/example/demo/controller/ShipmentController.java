e69cb59be39bc602c96ddf3a254f7a0cfd482257
PRADYUMNA KUMAR DAS <tech.daspk@gmail.com>
2019-10-06 23:05:52 +0530

    commit the changes

--- DroolRuleEngine/src/main/java/com/example/demo/controller/ShipmentController.java
+++ DroolRuleEngine/src/main/java/com/example/demo/controller/ShipmentController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +16,7 @@ import com.example.demo.model.Shipment;
 
 @RestController
 @RequestMapping("/start")
+@CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
 public class ShipmentController {
 
 	@Autowired
@@ -26,5 +31,17 @@ public class ShipmentController {
 	@RequestMapping("/test")
 	public String get() {
 		return "test";
+	}
+	@GetMapping("/shipments")
+	public List<Shipment> getShipments() {
+		
+		Shipment shipment1 = new Shipment("Mobile", 12, 12000);
+		Shipment shipment2 = new Shipment("Bike", 2, 123000);
+		Shipment shipment3 = new Shipment("Headphone", 19, 876);
+		List<Shipment> shipmentList = new ArrayList<Shipment>();
+		shipmentList.add(shipment1);
+		shipmentList.add(shipment2);
+		shipmentList.add(shipment3);
+		return shipmentList;
 	}
 }
